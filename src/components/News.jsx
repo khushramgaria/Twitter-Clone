8fb24e2f51996fa3cd843a01388f679dc2038510
Khushpreet Singh <khushramgaria@gmail.com>
2024-05-02 20:28:12 +0530

    responsive

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -26,18 +26,18 @@ function News() {
         },
     ]
   return (
-    <div className="third w-[70%]">
+    <div className="third w-[70%] hidden md:block">
         <div className="sticky top-0 pt-1">
         <div className="search flex items-center gap-4 m-2 ml-5 w-[70%] bg-[#202327] py-1 px-3 rounded-full">
             <FaSearch className='text-[#71767b] '/>
-            <input type="search" className='bg-transparent border-none outline-none placeholder:text-[#71767b]' placeholder='Search' />
+            <input type="search" className='bg-transparent  border-none outline-none placeholder:text-[#71767b]' placeholder='Search' />
         </div>
-        <div className="subscribe w-[70%] border mx-2 my-3 ml-5 rounded-xl p-2 pr-5">
+        <div className="subscribe w-[70%] border border-[#71767b] mx-2 my-3 ml-5 rounded-xl p-2 pr-5">
             <h6 className='font-bold'>Subscribe to Premium</h6>
             <p className='text-xs text-justify my-2'>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
             <button className='bg-[#1d9bf0] hover:bg-[#048de8] text-sm font-bold py-1 px-4 rounded-full mt-1'>Subscribe</button>
         </div>
-        <div className=" w-[70%] popular border ml-5 rounded-xl py-2 px-3">
+        <div className=" w-[70%] popular border border-[#71767b] ml-5 rounded-xl py-2 px-3">
             <h6 className="font-bold ">What’s happening</h6>
             {news.map((news) => (
                 <div className='flex justify-between mt-5'>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ import { IoMailOutline, IoPeopleOutline, IoPersonOutline } from "react-icons/io5
 import { LuClipboardList } from "react-icons/lu";
 import { FaXTwitter } from "react-icons/fa6";
 import { CgMoreO } from "react-icons/cg";
+import profileimg from "../img/profile.jpg"
 
 function Sidebar() {
     const navigator = [
@@ -20,28 +21,31 @@ function Sidebar() {
         { icon: <CgMoreO/>, name: "More" },
     ]
     return (
-    <div className="first w-[70%] ">
-        <div className='sticky top-0 pt-1'>
-        <div className="logo invert my-4">
-            <svg viewBox="0 0 24 24" aria-hidden="true" class="cursor-pointer mx-auto w-8 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
+    <div className="first w-10 md:w-[70%] ">
+        <div className='sticky top-0 pt-1 sm:pr-4'>
+        <div className="logo invert my-4 md:pl-8">
+            <svg viewBox="0 0 24 24" aria-hidden="true" class="cursor-pointer mx-auto w-7 md:w-6 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
         </div>
-        <div className="sidebar flex justify-end">
-            <ul className="flex flex-col text-[20px] space-y-3 px-[2.4rem]">
+        <div className="sidebar justify-center flex md:justify-end">
+            <ul className="flex flex-col text-xl md:text-lg space-y-2 md:px-[2.4rem]">
                 {navigator.map((navigate) => (
-                <li className="flex justify-start gap-3 items-center cursor-pointer w-fit hover:bg-[#202327] hover:rounded-xl px-3 py-1">{navigate.icon} {navigate.name}</li>
+                <li className="md:flex flex justify-center md:justify-start md:gap-3 items-center cursor-pointer md:w-fit hover:bg-[#202327] hover:rounded-xl px-3 py-1">{navigate.icon} <span className='hidden md:block'>{navigate.name}</span></li>
                 ))}
-                <li><button className="justify-start items-center cursor-pointer text-blue bg-[#1d9bf0] w-full rounded-3xl py-2 font-bold text-base hover:bg-[#048de8] mt-2 mb-6">Post</button></li>
-                <li className='flex justify-between items-center hover:bg-[#202327] hover:rounded-3xl px-3 py-1 my-8'>
+                <li>
+                    <button className="hidden md:block justify-start items-center cursor-pointer text-blue bg-[#1d9bf0] w-full rounded-3xl py-2 font-bold text-base hover:bg-[#048de8] mt-2 mb-6">Post</button>
+                    <CgMoreO className='md:hidden mx-auto bg-[#1d9bf0] text-[31px] p-[5px] mt-2 mb-6 rounded-full' />
+                </li>
+                <li className='flex justify-between items-center hover:bg-[#202327] hover:rounded-3xl px-2 py-1 my-8'>
                     <div className='flex gap-2'>
                         <div className="img">
-                            <img src="../img/profile.jpg" className="border w-full h-full rounded-full" />
+                            <img src={profileimg} className="md:w-8 rounded-full" />
                         </div>
-                        <div className="content">
+                        <div className="content hidden md:block">
                             <p className='text-xs'>Ramgaria</p>
                             <p className='text-xs text-slate-600'>@Ramgaria5</p>
                         </div>
                     </div>
-                    <div className='icon'>
+                    <div className='icon hidden md:block'>
                         <CgMoreO />
                     </div>
                 </li>
