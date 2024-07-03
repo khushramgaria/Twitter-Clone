3466297def5097845c6fa4c371cf55c932654b18
Khushpreet Singh <khushramgaria@gmail.com>
2024-07-03 23:08:14 +0530

    server change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v1"
+// export const server = "http://localhost:8000/api/v1"
 
-// export const server = "https://twitter-clone-backend-i7jj.onrender.com/api/v1"
+export const server = "https://twitter-clone-backend-i7jj.onrender.com/api/v1"
